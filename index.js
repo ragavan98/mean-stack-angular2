3face6b392ea15f7916cfd8bbe645c9df9ebe0cc
ragavan98 <ragavancool98@gmail.com>
2018-08-06 19:12:56 -0700

    server-setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database.js');
+const Path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err)=>{
+
+    if(err){
+        console.log('cannot connect to database');
+    }else{
+        
+        console.log('connected to database: '+ config.db);
+    }
+} );
+
+app.use(express.static(__dirname + '/client/dist/client/'));
+
+
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+});
+
+app.listen(8080, ()=>{
+    console.log('Listening on port 8080');
+});
